fcf28cbb2598e7923c14414e6001d77115bed649
Nina Weber <n.weber@marathon-berlin.de>
2019-11-20 17:07:11 +0100

    Use dotenv-webpack plugin package to support env vars in netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,50 @@
 /*eslint-env node*/
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
-const dotenv = require('dotenv')
+const Dotenv = require('dotenv-webpack')
 
-module.exports = () => {
-  const env = dotenv.config().parsed
-  const envVars = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next])
-    return prev
-  }, {})
+module.exports = () => ({
+  entry: './src/index.tsx',
 
-  return {
-    entry: './src/index.tsx',
-
-    module: {
-      rules: [
-        {
-          test: /\.(ts|js)x?$/,
-          exclude: /node_modules/,
-          use: ['babel-loader'],
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.styl$/,
-          use: ['style-loader', 'css-loader', 'stylus-loader'],
-        },
-      ],
-    },
-
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js'],
-    },
-
-    output: {
-      path: __dirname + '/dist',
-      publicPath: '/',
-      filename: 'bundle.js',
-    },
-
-    devServer: {
-      contentBase: './dist',
-      historyApiFallback: true,
-    },
-
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-
-      new webpack.DefinePlugin(envVars),
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader'],
+      },
     ],
-  }
-}
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+
+    new Dotenv({
+      systemvars: true,
+    }),
+  ],
+})
