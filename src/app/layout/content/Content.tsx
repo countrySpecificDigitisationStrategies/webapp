61f8c0c3cc4459b4a8ae862e9bbf2766418dbb6e
Nina Weber <n.weber@marathon-berlin.de>
2019-12-05 11:38:21 +0100

    Add minimal Breadcrumb

--- src/app/layout/content/Content.tsx
+++ src/app/layout/content/Content.tsx
@@ -5,9 +5,11 @@ import { Route } from 'react-router-dom'
 
 import { Home, Login, Register, Strategies, Strategy } from 'pages'
 import { withErrorNotification, withLoadingOverlay } from 'shared/hocs'
+import { Breadcrumb } from 'shared/components/breadcrumb/Breadcrumb'
 
 const Content = (): JSXElement => (
   <>
+    <Route path="/:path" component={Breadcrumb} />
     <Route exact path={APP_ROUTES.home} component={Home} />
     <Route exact path={APP_ROUTES.login} component={Login} />
     <Route exact path={APP_ROUTES.register} component={Register} />
