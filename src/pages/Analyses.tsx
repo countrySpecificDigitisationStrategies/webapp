7a88215d59f89da1e127f9181ce515b5e0236c6b
Max Franke <mfranke@inet.tu-berlin.de>
2020-01-09 15:57:40 +0100

    Nits

--- src/pages/Analyses.tsx
+++ src/pages/Analyses.tsx
@@ -3,9 +3,6 @@ import { AnalysesGrid } from 'features/analyses'
 import StandardView from 'shared/components/standard-view/StandardView'
 
 const Analyses = () => {
-  const LoremIpsum =
-    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
-
   const renderAnalysesGrid = () => <AnalysesGrid />
   return (
     <StandardView
