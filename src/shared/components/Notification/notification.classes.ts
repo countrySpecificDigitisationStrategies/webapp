ba259da3c4de2a4aac998c41d44474296a51d2a9
Nina Weber <n.weber@marathon-berlin.de>
2020-01-11 15:21:39 +0100

    Fix/ignore 2 remaining errors

--- src/shared/components/Notification/notification.classes.ts
+++ src/shared/components/Notification/notification.classes.ts
@@ -2,27 +2,12 @@ import { makeStyles, Theme } from '@material-ui/core'
 import { amber, green } from '@material-ui/core/colors'
 import { NotificationType } from 'shared/components/Notification/Notification'
 
-type ColorMap<T> = { [type in NotificationType]: T }
-
-const getColors = (theme: Theme): ColorMap<string> => ({
-  [NotificationType.success]: green[600],
-  [NotificationType.warning]: amber[700],
-  [NotificationType.error]: theme.palette.error.dark,
-  [NotificationType.info]: theme.palette.primary.main,
-})
-
 export const notificationStyles = makeStyles((theme: Theme) => {
-  const colors = getColors(theme)
-  const notificationClasses = (Object.keys(colors) as NotificationType[]).reduce<ColorMap<object>>(
-    (acc, type: NotificationType) => {
-      acc[type] = { backgroundColor: colors[type] }
-      return acc
-    },
-    {} as ColorMap<object>
-  )
-
   return {
-    ...(notificationClasses as { [notificationType: string]: object }),
+    [NotificationType.success]: { backgroundColor: green[600] },
+    [NotificationType.warning]: { backgroundColor: amber[700] },
+    [NotificationType.error]: { backgroundColor: theme.palette.error.dark },
+    [NotificationType.info]: { backgroundColor: theme.palette.primary.main },
     typeIcon: {
       opacity: 0.9,
       marginRight: theme.spacing(1),
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,8 +1,12 @@
-export const toIndexedObject = <T extends object>(array: T[], index: keyof T): { [key in keyof T]: T } =>
+// TODO: Fix typing for this function
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+type indexedObject<T> = { [index in T[keyof T]]: T }
+export const toIndexedObject = <T extends object>(array: T[], index: keyof T): indexedObject<T> =>
   array.reduce((acc, curr) => {
     acc[curr[index]] = curr
     return acc
-  }, {} as { [key in keyof T]: T })
+  }, {} as indexedObject<T>)
 
 export const addToState = <T extends object>(state: T, key: keyof T, payload: object): T => ({
   ...state,
