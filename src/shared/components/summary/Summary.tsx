6b6af4cdd327b95440d106075e0b50b76f2278ef
Nina Weber <n.weber@marathon-berlin.de>
2020-01-17 14:00:40 +0100

    Improve appearance of Summary Component

--- src/shared/components/summary/Summary.tsx
+++ src/shared/components/summary/Summary.tsx
@@ -1,34 +1,37 @@
-import React from 'react'
-import { ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Typography } from '@material-ui/core'
-import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons'
+import React, { useState } from 'react'
+import { IconButton, Typography } from '@material-ui/core'
+import { ExpandMore as ExpandMoreIcon, ExpandLess as ExpandLessIcon } from '@material-ui/icons'
 
 interface SummaryProps {
   text: string
   maxWords?: number
 }
 
+const cssClassName = 'summary'
+
 export const Summary = ({ text, maxWords = 50 }: SummaryProps) => {
+  const [expanded, setExpanded] = useState(false)
   const words = text.split(' ')
 
   /** Text is short enough to not need a summary */
   if (words.length < maxWords) {
     return (
-      <ExpansionPanel defaultExpanded>
-        <ExpansionPanelDetails>
-          <Typography>{text}</Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+      <div>
+        <Typography>{text}</Typography>
+      </div>
     )
   }
 
+  const summary = words.slice(0, maxWords).join(' ') + '...'
+
   return (
-    <ExpansionPanel defaultExpanded={false}>
-      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography>{words.slice(0, maxWords).join(' ')}...</Typography>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails>
-        <Typography>{text}</Typography>
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
+    <div>
+      <Typography>{expanded ? text : summary}</Typography>
+      <div className={`${cssClassName}__button-wrapper`}>
+        <IconButton onClick={() => setExpanded(!expanded)}>
+          {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        </IconButton>
+      </div>
+    </div>
   )
 }
