2cda42ef66705e88713dd81b437a65474572f7b0
Nina Weber <n.weber@marathon-berlin.de>
2020-01-19 23:17:02 +0100

    Re-enabled source-maps with faster option in development and enabled babel cache for faster build times

--- webpack.config.js
+++ webpack.config.js
@@ -5,65 +5,65 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
 
-module.exports = () => ({
-  entry: './src/index.tsx',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
 
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.styl$/,
-        use: ['style-loader', 'css-loader', 'stylus-loader'],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              disable: true,
+  return {
+    entry: './src/index.tsx',
+
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: ['babel-loader?cacheDirectory'],
+        },
+        {
+          test: /\.(css|styl)$/,
+          use: ['style-loader', 'css-loader', 'stylus-loader'],
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                disable: true,
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      modules: [path.resolve(__dirname, './src'), path.resolve(__dirname, './node_modules')],
+    },
+
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js',
+    },
+
+    devServer: {
+      contentBase: './dist',
+      historyApiFallback: true,
+    },
+
+    devtool: isProduction ? 'source-map' : 'eval-cheap-source-map',
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+      }),
+
+      new Dotenv({
+        systemvars: true,
+      }),
     ],
-  },
-
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    modules: [path.resolve(__dirname, './src'), path.resolve(__dirname, './node_modules')],
-  },
-
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-
-  devServer: {
-    contentBase: './dist',
-    historyApiFallback: true,
-  },
-
-  // devtool: 'source-map',
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-
-    new Dotenv({
-      systemvars: true,
-    }),
-  ],
-})
+  }
+}
