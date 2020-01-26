fcdaf62dfb92fe3971ea284724a2c81a0585b588
Nina Weber <n.weber@marathon-berlin.de>
2020-01-26 17:23:24 +0100

    Fix Strategy Types to match new API, including the IT-Board

--- src/features/strategies/store/actions.strategies.ts
+++ src/features/strategies/store/actions.strategies.ts
@@ -1,7 +1,7 @@
-import { Country, Strategy, User } from './types'
+import { Country, Strategy } from './types'
 import { Endpoint, get } from 'app/service'
 import { createRequest } from 'features/requests/store'
-import { CountryResponse, StrategyResponse, UserResponse } from 'features/strategies/store/types.api'
+import { CountryResponse, StrategyResponse } from 'features/strategies/store/types.api'
 
 export const STRATEGIES_REQUEST_ID = 'strategies'
 export const STRATEGIES_ADD = 'strategies/add'
@@ -27,10 +27,9 @@ const addStrategies = (strategies: Strategy[]): StrategiesAdd => ({
 })
 
 const transformResponseData = (strategies: StrategyResponse[]): Strategy[] => {
-  return strategies.map(({ user, country, buildingBlocks, situationCategories, created, updated, ...strategy }) => ({
+  return strategies.map(({ board, buildingBlocks, situationCategories, created, updated, ...strategy }) => ({
     ...strategy,
-    user: transformUserData(user),
-    country: transformCountryData(country),
+    country: transformCountryData(board.country),
     blocks: buildingBlocks,
     categories: situationCategories,
     created: new Date(created),
@@ -39,25 +38,25 @@ const transformResponseData = (strategies: StrategyResponse[]): Strategy[] => {
 }
 
 // TODO: should be defined elsewhere
-const transformUserData = ({
-  firstname,
-  lastname,
-  country,
-  currentCountry,
-  created,
-  updated,
-  ...user
-}: UserResponse): User => {
-  return {
-    ...user,
-    firstName: firstname,
-    lastName: lastname,
-    country: country ? transformCountryData(country) : null,
-    currentCountry: currentCountry ? transformCountryData(currentCountry) : null,
-    created: new Date(created),
-    updated: new Date(updated),
-  }
-}
+// const transformUserData = ({
+//   firstname,
+//   lastname,
+//   country,
+//   currentCountry,
+//   created,
+//   updated,
+//   ...user
+// }: UserResponse): User => {
+//   return {
+//     ...user,
+//     firstName: firstname,
+//     lastName: lastname,
+//     country: country ? transformCountryData(country) : null,
+//     currentCountry: currentCountry ? transformCountryData(currentCountry) : null,
+//     created: new Date(created),
+//     updated: new Date(updated),
+//   }
+// }
 
 // TODO: should be defined elsewhere
 const transformCountryData = ({ created, updated, ...country }: CountryResponse): Country => {
