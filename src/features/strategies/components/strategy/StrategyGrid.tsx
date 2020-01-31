6f2fce90678f9d8e86e767756f5204ff3dc5913e
Nina Weber <n.weber@marathon-berlin.de>
2020-01-31 12:26:48 +0100

    Add strategy titles in StrategyGrid

--- src/features/strategies/components/strategy/StrategyGrid.tsx
+++ src/features/strategies/components/strategy/StrategyGrid.tsx
@@ -15,13 +15,25 @@ const StrategyGrid = ({ linkTo = APP_ROUTES.strategy }: StrategyGridProps): JSX.
   const strategies = Object.values(useSelector(getStrategies) || {})
   const countryIds = strategies.filter(strategy => strategy.isPublished).map(strategy => strategy.country.id)
 
+  const getStrategyByCountryId = (countryId: Country['id']) =>
+    strategies.find(strategy => strategy.country.id === countryId)
+
   const getStrategyRoute = (countryId: Country['id']) => {
-    const strategyId = strategies.find(strategy => strategy.country.id === countryId)?.id
+    const strategyId = getStrategyByCountryId(countryId)?.id
     if (strategyId) return linkTo.replace(`:${APP_ROUTE_PARAMS.strategyId}`, String(strategyId))
     else return ''
   }
 
-  return <CountryGrid ids={countryIds} cardLink={getStrategyRoute} emptyMessage="Sorry, there are no Strategies yet." />
+  const getStrategyTitle = (countryId: Country['id']) => getStrategyByCountryId(countryId)?.title
+
+  return (
+    <CountryGrid
+      ids={countryIds}
+      cardLink={getStrategyRoute}
+      cardTitle={getStrategyTitle}
+      emptyMessage="Sorry, there are no Strategies yet."
+    />
+  )
 }
 
 export default StrategyGrid
