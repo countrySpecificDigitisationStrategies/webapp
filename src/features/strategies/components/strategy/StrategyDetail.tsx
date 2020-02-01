43be726f386d2118fee7bb0692b3dd13062a4a33
Nina Weber <n.weber@marathon-berlin.de>
2020-02-01 15:38:04 +0100

    Integrate discussion preview in StrategyEntity-views

--- src/features/strategies/components/strategy/StrategyDetail.tsx
+++ src/features/strategies/components/strategy/StrategyDetail.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Button, Typography } from '@material-ui/core'
 import { getStrategy, Strategy } from 'features/strategies/store'
-import { BlockGrid, useStrategyData } from 'features/strategies/components'
-import { StandardView } from 'shared/components'
+import { BlockGrid, EntityDetailView, EntityType, useStrategyData } from 'features/strategies/components'
 
 interface StrategyDetailProps {
   id: Strategy['id']
@@ -14,23 +13,25 @@ const StrategyDetail = ({ id }: StrategyDetailProps) => {
   const strategy = useSelector(getStrategy(id))
   if (!strategy) return <div>Could not find Strategy with id {id}</div>
 
-  const analysisFragment = (
-    <>
-      <Typography variant="h5" className="strategy-detail__subheading">
-        Analysis
-      </Typography>
-      <Button color="primary" variant="contained">
-        Show complete analysis
-      </Button>
-    </>
-  )
-
   const renderBlockGrid = () => <BlockGrid ids={strategy.blocks} />
   return (
-    <StandardView
-      title={strategy.country.name}
+    <EntityDetailView
+      entityType={EntityType.Strategy}
+      entityId={id}
+      title={strategy.title}
+      subtitle={strategy.country.name}
       description={strategy.description}
-      renderAdditionalInfo={() => analysisFragment}
+      renderInfo={() => (
+        //TODO: Move to Analyses Feature
+        <>
+          <Typography variant="h5" className="strategy-detail__subheading">
+            Analysis
+          </Typography>
+          <Button color="primary" variant="contained">
+            Show complete analysis
+          </Button>
+        </>
+      )}
       nextLevel={{
         title: 'Building Blocks',
         render: renderBlockGrid,
--- src/shared/components/standard-view/StandardView.tsx
+++ src/shared/components/standard-view/StandardView.tsx
@@ -2,51 +2,33 @@ import React from 'react'
 import { Typography } from '@material-ui/core'
 import { Markdown } from 'shared/components'
 
-interface DetailViewProps {
+export interface StandardViewProps {
   title?: string
+  subtitle?: string
   description?: string
-  renderAdditionalInfo?: () => JSX.Element
-  nextLevel?: {
-    title?: string
-    render: () => JSX.Element
-  }
+  renderContent?: () => JSX.Element
 }
 
-export const StandardView = ({ title, description, renderAdditionalInfo, nextLevel }: DetailViewProps) => {
+export const StandardView = ({ title, subtitle, description, renderContent }: StandardViewProps) => {
   const className = 'detail-view'
-  const titleFragment = title ? (
-    <Typography variant="h3" className={`${className}__heading`}>
-      {title}
-    </Typography>
-  ) : null
-
-  const descriptionFragment = description ? (
-    <div className={`${className}__description`}>
-      <Markdown markdown={description} />
-    </div>
-  ) : null
-
-  const additionalInfoFragment = renderAdditionalInfo ? (
-    <div className={`${className}__additional-info`}>{renderAdditionalInfo()}</div>
-  ) : null
-
-  const nextLevelFragment = nextLevel ? (
-    <div className={`${className}__next-level`}>
-      {nextLevel.title && (
-        <Typography variant="h5" className="strategy-detail__subheading">
-          {nextLevel.title}
-        </Typography>
-      )}
-      {nextLevel.render()}
-    </div>
-  ) : null
-
   return (
     <div className={className}>
-      {titleFragment}
-      {descriptionFragment}
-      {additionalInfoFragment}
-      {nextLevelFragment}
+      {subtitle && (
+        <Typography variant="overline" className={`${className}__overline`}>
+          {subtitle}
+        </Typography>
+      )}
+      {title && (
+        <Typography variant="h3" className={`${className}__heading`}>
+          {title}
+        </Typography>
+      )}
+      {description && (
+        <div className={`${className}__description`}>
+          <Markdown markdown={description} />
+        </div>
+      )}
+      {renderContent && <div className={`${className}__additional-info`}>{renderContent()}</div>}
     </div>
   )
 }
